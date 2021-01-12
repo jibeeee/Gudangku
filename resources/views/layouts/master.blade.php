ecfd1c60448d061bab997b2a4b371131d5fe1d76
tanfirul_roy <tanfirulroy@student.ittlkom-sby.ac.id>
2021-01-12 23:19:09 +0700

    tambah navbar

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>Gudangku</title>
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Gudangku</a>
+            <div class="d-flex">
+                <ul class="navbar-nav nav justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                </ul>
+                <a class="nav-link" href="#">Services</a>
+                <button class="btn btn-outline-success">Search</button>
+            </div>
+        </div>
+    </nav>
+
+</body>
+</html>
