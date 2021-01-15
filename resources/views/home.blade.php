6e0cbffa0650acd70248984263d3e9341003abaa
tanfirul_roy <tanfirulroibafi502@gmail.com>
2021-01-15 19:39:18 +0700

    update home screen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,27 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="container">
-    
-</div>
+<link href="{{ asset('css/home.css') }}" rel="stylesheet"/>
+
+    <div class='home-section'>
+        <div class='container'>
+            <div class='row home-row'>
+                <div class='col'>
+                    <div class='home-text-wrapper'>
+                        <h1 class='heading'>GudangKu</h1>
+                        <div class='top-line'>Pertama di Indonesia</div>
+                        <p class='home-subtitle'>
+                            Pencatatan keluar masuk gudang menjadi lebih aman dan mudah, tanpa ribet dan tapi-tapian
+                        </p>
+                        <a href="/service" class="btn--primary">Learn More</a>
+                    </div>
+                </div>
+                <div class='col'>
+                    <div class='home-img-wrapper'>
+                        <img src="{{ asset('img/undraw_logistics_x4dc.svg') }}" alt="ilustrasi gudang" class='home-img' />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="{{ URL::asset('css/master.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/master.css') }}" rel="stylesheet"/>
+    
     <title>Gudangku</title>
 </head>
 <body>
